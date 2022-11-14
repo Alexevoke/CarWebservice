d7274f5c54650c4b0745f5f0986745078acc0477
Alexander Gerdil <alex650w@edu.zealand.dk>
2022-11-14 15:23:19 +0100

    can get all cars from the api

--- js/index.js
+++ js/index.js
@@ -1,13 +1,47 @@
+const url = 'https://webapicar20190326034339.azurewebsites.net/api/cars'
+
 const app = Vue.createApp({
     data() {
         return {
             // add data here
+            cars: []
         }
         
     },
 
     methods: {
-        // add methods here
+        GetAllCars(){
+            axios.get(url)
+            .then(response => {
+                console.log(response.status)
+                console.log(response.statusText)
+                console.log('*****')
+
+                this.cars = response.data
+
+                this.cars.forEach(element => {
+                    console.log(element)                    
+                });
+                console.log('*****')
+
+            })
+            .catch(error =>{
+                if (error.response) {
+                    console.log(error.response.status)
+                    console.log(error.response.statusText)
+                    console.log('*****')
+                }
+                else if (error.request) {
+                    console.log(error.request)
+                }
+                else {
+                    console.log("Error", error.message)
+                }
+            })
+            
+
+            
+        }
     }
     
 })
